013288a5f76bf3937056bd9c4e7786e8119bb8f8
Vrezh <hovanisyan.vrezh@gmail.com>
2018-06-30 14:20:58 +0400

    [add] import createtodo

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import injectSheet from 'react-jss';
+
+import CreateToDo from '../CreateToDo';
+
 import style from './style';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: []
+    };
+
+    this.onToDoItemCreate = this.onToDoItemCreate.bind(this);
+  }
+
+  onToDoItemCreate(value) {
+    const { todo } = this.state;
+
+    this.setState({
+      todo: [...todo, value]
+    });
+  }
+
   render() {
     const { classes } = this.props;
+    const { todo } = this.state;
+
     return (
       <div className={classes.root}>
         <h1 className={classes.title}>todos</h1>
+        <CreateToDo onToDoItemCreate={this.onToDoItemCreate} />
+        <ul>{todo.map((value, idx) => <li key={idx}>{value}</li>)}</ul>
       </div>
     );
   }

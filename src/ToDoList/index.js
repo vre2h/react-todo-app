a407f34fda3752c7b85d985843a6b2d661511204
Vrezh <hovanisyan.vrezh@gmail.com>
2018-06-30 19:53:39 +0400

    [add] init todolist component

--- src/ToDoList/index.js
+++ src/ToDoList/index.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import style from './style';
 import injectSheet from 'react-jss';
+
+class ToDoList extends React.Component {
+  render() {
+    const { todo, classes } = this.props;
+    return (
+      <div>
+        <ul>{todo.map((value, idx) => <li key={idx}>{value}</li>)}</ul>
+        <div className={classes.root}>
+          <span>Items Left</span>
+          <div className={classes.filter}>
+            <button> All </button>
+            <button> Completed </button>
+            <button> Active </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default injectSheet(style)(ToDoList);
---
+++ src/ToDoList/style.js
@@ -0,0 +1,12 @@
+export default {
+  root: {
+    display: 'flex',
+    alignItems: 'stretch',
+    flexDirection: 'column'
+  },
+  filter: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'space-between'
+  }
+};

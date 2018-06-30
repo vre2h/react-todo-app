a57e1bc714cacae182e1faf1e9844959eaeccf64
Vrezh <hovanisyan.vrezh@gmail.com>
2018-06-30 20:00:40 +0400

    [del] margins for main-title

--- src/Main/style.js
+++ src/Main/style.js
@@ -11,6 +11,7 @@ export default {
     lineHeight: '100px',
     fontSize: '100px',
     fontWeight: '300',
-    color: 'rgba(175, 47, 47, 0.15);'
+    color: 'rgba(175, 47, 47, 0.15);',
+    margin: '0'
   }
 };

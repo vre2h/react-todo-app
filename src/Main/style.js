caac78bf0b103846c47392b130cb8927b4860179
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-01 13:51:57 +0400

    [add] array of colors

--- src/Main/style.js
+++ src/Main/style.js
@@ -1,3 +1,9 @@
+const titleColors = [
+  'rgba(175, 47, 47, 0.15)',
+  'rgba(93, 194, 175, 0.55)',
+  'rgba(119, 119, 119, 0.45)'
+];
+
 export default {
   root: {
     display: 'flex',
@@ -7,12 +13,18 @@ export default {
     width: '100vw',
     height: '100vh'
   },
+
   title: {
-    lineHeight: '170px',
+    lineHeight: '150px',
     fontSize: '170px',
     fontWeight: '300',
-    color: 'rgba(175, 47, 47, 0.15);',
-    margin: '0'
+    color: titleColors[1],
+    margin: '10px 0',
+    cursor: 'pointer',
+    transition: '.3s',
+    '&:hover': {
+      color: titleColors[0]
+    }
   },
   wrapper: {
     width: '320px',

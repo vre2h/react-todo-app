6eee957485da1ab549892d66f1222d68971dc76b
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-01 13:53:03 +0400

    [add] styles and layout for todo items

--- src/ToDoList/index.js
+++ src/ToDoList/index.js
@@ -6,14 +6,27 @@ class ToDoList extends React.Component {
   render() {
     const { todo, classes } = this.props;
     return (
-      <div>
-        <ul>{todo.map((value, idx) => <li key={idx}>{value}</li>)}</ul>
+      <div className={classes.list__wrapper}>
+        <ul className={classes.list}>
+          {todo.map((value, idx) => (
+            <li className={classes.list__item} key={idx}>
+              <input
+                type="checkbox"
+                id="list__item-input"
+                className={classes.list__checkbox}
+              />
+              <label className={classes.list__label} htmlFor="list__item-input">
+                {value}
+              </label>
+            </li>
+          ))}
+        </ul>
         <div className={classes.root}>
-          <span>Items Left</span>
+          <span>Items Left: {}</span>
           <div className={classes.filter}>
-            <button> All </button>
-            <button> Completed </button>
-            <button> Active </button>
+            <button className={classes.filter__btn}> All </button>
+            <button className={classes.filter__btn}> Completed </button>
+            <button className={classes.filter__btn}> Active </button>
           </div>
         </div>
       </div>
--- src/ToDoList/style.js
+++ src/ToDoList/style.js
@@ -1,12 +1,63 @@
 export default {
   root: {
     display: 'flex',
-    alignItems: 'stretch',
-    flexDirection: 'column'
+    alignItems: 'center',
+    flexDirection: 'row',
+    margin: '20px 0',
+    justifyContent: 'space-between'
   },
+
   filter: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'space-between'
+    alignItems: 'space-between',
+    justifyContent: 'center'
+  },
+
+  list: {
+    listStyle: 'none',
+    margin: '0',
+    padding: '0'
+  },
+
+  list__wrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+
+  list__item: {
+    padding: '7px 14px 10px 14px',
+    fontSize: '18px',
+    border: '1px solid #e9e9e9',
+    borderTop: '0',
+    cursor: 'pointer',
+    color: '#212121'
+  },
+
+  list__label: {
+    margin: '0 10px'
+  },
+
+  list__checkbox: {
+    '&:checked + label': {
+      textDecoration: 'line-through'
+    }
+  },
+
+  filter__btn: {
+    padding: ' 7px 14px',
+    marginLeft: '7px',
+    border: '1px solid #e9e9e9',
+    background: 'transparent',
+    cursor: 'pointer',
+    transition: '.3s',
+    fontFamily: "'Yanone Kaffeesatz', sans-serif",
+    cursor: 'pointer',
+    fontSize: '18px',
+    '&:hover': {
+      color: '#fff',
+      backgroundColor: '#212121'
+    }
   }
 };

69199c3eac4366e7ff50401a17b686ee5d0f3f6a
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-01 19:27:22 +0400

    [fix] handleChange warning

--- src/ToDoList/index.js
+++ src/ToDoList/index.js
@@ -9,15 +9,26 @@ class ToDoList extends React.Component {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleClick(todo, e) {
-    this.props.onToDoItemRemover(markToDo(todo, this.props.todos));
+    this.handleChange(todo);
+  }
+
+  handleChange(todo) {
+    console.log('a');
+    this.props.handleToDoItemMaker(markToDo(todo, this.props.todos));
   }
 
   render() {
     const { classes } = this.props;
     const { todos } = this.props;
+
+    const leftItems = todos.reduce(
+      (acc, { value, isComplete }) => (isComplete === false ? (acc += 1) : acc),
+      0
+    );
 
     return (
       <div className={classes.list__wrapper}>
@@ -26,22 +37,40 @@ class ToDoList extends React.Component {
             <li
               className={classes.list__item}
               key={idx}
-              onClick={this.handleClick.bind(this, { value, isComplete })}
+              onClick={this.handleClick.bind(this, {
+                value,
+                isComplete,
+                idx
+              })}
             >
               <input
                 type="checkbox"
                 id="list__item-input"
                 className={classes.list__checkbox}
                 checked={isComplete}
-              />
+                onChange={this.handleChange.bind(this, {
+                  value,
+                  isComplete,
+                  idx
+                })}
+              />{' '}
               <label className={classes.list__label} htmlFor="list__item-input">
                 {value}
               </label>
+              <span className={classes.customCheckbox}>
+                <span
+                  className={
+                    isComplete ? classes['customCheckbox--mark'] : null
+                  }
+                >
+                  &#10003;
+                </span>
+              </span>
             </li>
           ))}
         </ul>
         <div className={classes.root}>
-          <span>Items Left: {}</span>
+          <span>Tasks Left: {leftItems}</span>
           <div className={classes.filter}>
             <button className={classes.filter__btn}> All </button>
             <button className={classes.filter__btn}> Completed </button>
--- src/ToDoList/style.js
+++ src/ToDoList/style.js
@@ -27,7 +27,10 @@ export default {
   },
 
   list__item: {
-    padding: '7px 14px 10px 14px',
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '15px',
     fontSize: '18px',
     border: '1px solid #e9e9e9',
     borderTop: '0',
@@ -36,14 +39,39 @@ export default {
   },
 
   list__label: {
-    margin: '0 10px'
+    position: 'relative',
+    zIndex: '-100',
+    margin: '0 10px',
+    cursor: 'pointer',
+    marginLeft: '20px'
   },
 
   list__checkbox: {
+    position: 'relative',
+    display: 'block',
+    '-webkit-appearance': 'none',
+    cursor: 'pointer',
     '&:checked + label': {
       textDecoration: 'line-through',
       color: '#ccc'
     }
+  },
+
+  customCheckbox: {
+    order: '-1',
+    width: '25px',
+    height: '25px',
+    border: ' 1px solid #e9e9e9',
+    borderRadius: '50%',
+    fontSize: 0,
+    transition: '.2s'
+  },
+
+  'customCheckbox--mark': {
+    display: 'block',
+    marginTop: '-18px',
+    fontSize: '45px',
+    color: '#3a3a3a'
   },
 
   filter__btn: {

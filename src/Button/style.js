fbbdfbe50c034208265c3e2a4e008a979bbe1ddb
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-04 16:57:41 +0400

    [add] styles for active filtered btn

--- src/Button/style.js
+++ src/Button/style.js
@@ -2,10 +2,15 @@ export default {
   default: {
     fontFamily: "'Yanone Kaffeesatz', sans-serif",
     fontSize: '18px',
+    backgroundColor: 'transparent',
     transition: '.5s',
     cursor: 'pointer',
     '&:hover': {
       color: 'burlywood'
     }
+  },
+
+  'default--active': {
+    transform: 'translateY(-10px)'
   }
 };

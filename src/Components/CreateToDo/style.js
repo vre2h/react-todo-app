8a4d9e586ca5e34dc6f0cf1821026fe116c2421e
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-06 16:34:44 +0400

    [add] padding for input

--- src/Components/CreateToDo/style.js
+++ src/Components/CreateToDo/style.js
@@ -10,7 +10,7 @@ export default {
   input: {
     flexGrow: '1',
     padding: '10px',
-    paddingLeft: '40px',
+    paddingLeft: '50px',
     border: '1px solid #e9e9e9',
     borderRight: 'none',
     fontFamily: "'Yanone Kaffeesatz', sans-serif",
@@ -42,21 +42,22 @@ export default {
     '-ms-appearance': 'none',
     '-o-appearance': 'none',
     appearance: 'none',
-    margin: '0',
-    '&::before': {
-      position: 'absolute',
-      content: '"❯"',
-      fontSize: '22px',
-      color: '#212121',
-      cursor: 'pointer',
-      transition: '.2s',
-      marginLeft: '12px'
-    }
+    margin: '0'
   },
 
-  'toggleAll-down': {
-    '&::before': {
-      transform: 'rotate(90deg)'
-    }
+  'toggleAll-custom': {
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: '-3px',
+    fontSize: '22px',
+    color: '#212121',
+    cursor: 'pointer',
+    transition: '.2s',
+    marginLeft: '22px'
+  },
+
+  'toggleAll-custom--down': {
+    marginTop: '0',
+    transform: 'rotate(90deg)'
   }
 };
